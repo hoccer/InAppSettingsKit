886fd3e7b37cfe9973b5afa6ef09df498ebc5921
Pavel Mayer <pavel@hoccer.com>
2013-06-15 17:42:54 +0200

    - removed workarounds and deprecated API usage

--- InAppSettingsKit/Controllers/IASKAppSettingsViewController.m
+++ InAppSettingsKit/Controllers/IASKAppSettingsViewController.m
@@ -490,8 +490,10 @@ CGRect IASKCGRectSwap(CGRect rect);
 	} else {
 		cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
 	}
-	cell.textLabel.minimumFontSize = kIASKMinimumFontSize;
-	cell.detailTextLabel.minimumFontSize = kIASKMinimumFontSize;
+	//cell.textLabel.minimumFontSize = kIASKMinimumFontSize;
+	//cell.detailTextLabel.minimumFontSize = kIASKMinimumFontSize;
+	cell.textLabel.minimumScaleFactor = 0.5;
+	cell.detailTextLabel.minimumScaleFactor = 0.5;
 	return cell;
 }
 
@@ -778,19 +780,10 @@ CGRect IASKCGRectSwap(CGRect rect);
             }
             
             mailViewController.mailComposeDelegate = vc;
-#if (__IPHONE_OS_VERSION_MIN_REQUIRED >= 50000)
-#pragma message "Now that we're iOS5 and up, remove this workaround"
-#endif
-            if([vc respondsToSelector:@selector(presentViewController:animated:completion:)]) {
-                [vc presentViewController:mailViewController
-                                   animated:YES
-                                 completion:nil];
-            } else {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-                [vc presentModalViewController:mailViewController animated:YES];
-#pragma clang diagnostic pop
-            }
+            
+            [vc presentViewController:mailViewController
+                             animated:YES
+                           completion:nil];
             [mailViewController release];
         } else {
             UIAlertView *alert = [[UIAlertView alloc]
@@ -822,21 +815,9 @@ CGRect IASKCGRectSwap(CGRect rect);
                          mailComposeController:controller 
                            didFinishWithResult:result 
                                          error:error];
-     }
-    
-#if (__IPHONE_OS_VERSION_MIN_REQUIRED >= 50000)
-#pragma message "Now that we're iOS5 and up, remove this workaround"
-#endif
-    if([self respondsToSelector:@selector(dismissViewControllerAnimated:completion:)]) {
-        [self dismissViewControllerAnimated:YES
-                                 completion:nil];
-    } else {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-        [self dismissModalViewControllerAnimated:YES];
-#pragma clang diagnostic pop
-        
     }
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
 }
 
 #pragma mark -
